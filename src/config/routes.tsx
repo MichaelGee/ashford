d73cca0e17e4767a93676de73ba35accc92c0ffc
Michael Ekwere <michaelgatesjr@gmail.com>
2024-03-23 15:03:24 +0100

    feat: create verify account page

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,12 +1,13 @@
 import CreateAccount from '@/pages/createAccount/createAccount';
 import Login from '@/pages/login/login';
 import LoginAsGuest from '@/pages/loginAsGuest/loginAsGuest';
+import VerifyAccount from '@/pages/verifyAccount/verifyAccount';
 
 const authRoutes = [
   {path: '/login', element: <Login />},
   {path: '/create-account', element: <CreateAccount />},
   {path: '/login-as-guest', element: <LoginAsGuest />},
-  // {path: '/verify-code', element: <VerifyCode />},
+  {path: '/verify-account', element: <VerifyAccount />},
 ];
 
 const inAppRoutes = [];
---
+++ src/pages/verifyAccount/verifyAccount.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {Button} from '@/components/ui/button';
+import Google from '@/assets/images/google.svg';
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from '@/components/ui/input-otp';
+
+const VerifyAccount = () => {
+  return (
+    <React.Fragment>
+      <div className="mb-space600">
+        <h1 className="text-[1.5rem] text-primary font-bold">Enter OTP</h1>
+        <p className="text-[0.8rem] text-gray002">
+          Enter 6-digit OTP sent to you
+        </p>
+      </div>
+      <form className="flex flex-col justify-between h-[65vh]">
+        <div className="flex flex-col gap-space200 ">
+          <InputOTP maxLength={6}>
+            <InputOTPGroup>
+              <InputOTPSlot index={0} />
+              <InputOTPSlot index={1} />
+              <InputOTPSlot index={2} />
+            </InputOTPGroup>
+            <InputOTPSeparator />
+            <InputOTPGroup>
+              <InputOTPSlot index={3} />
+              <InputOTPSlot index={4} />
+              <InputOTPSlot index={5} />
+            </InputOTPGroup>
+          </InputOTP>
+          <Button className="w-full">Submit</Button>
+          <p className="text-[0.8rem] leading-4 text-gray002">
+            Resend code in
+            <span className="text-blue001"> 04:46</span>
+          </p>
+        </div>
+        <div className="flex flex-col items-center justify-center text-center gap-space100">
+          <Button className="w-fit bg-transparent" variant="outline">
+            <img src={Google} className="mr-3" /> Sign in with Google
+          </Button>
+          <span className="text-[0.8rem]">
+            New user? <a>Sign up</a>
+          </span>
+        </div>
+      </form>
+    </React.Fragment>
+  );
+};
+
+export default VerifyAccount;
