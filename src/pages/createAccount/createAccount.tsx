3b5dc8be87604e1af264c954927b55be61f298e5
Michael Ekwere <michaelgatesjr@gmail.com>
2024-03-27 13:05:27 +0100

    feat: signup form validation

--- src/pages/createAccount/createAccount.tsx
+++ src/pages/createAccount/createAccount.tsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button} from '@/components/ui/button';
 import {Input} from '@/components/ui/input';
 import {Checkbox} from '@/components/ui/checkbox';
 
+import {useForm, SubmitHandler, Controller} from 'react-hook-form';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {z} from 'zod';
+
+const schema = z
+  .object({
+    firstName: z.string().min(1, 'First name is required'),
+    lastName: z.string().min(1, 'Last name is required'),
+    email: z.string().email(),
+    phoneNumber: z.string().max(10).min(10),
+    pin: z.string().max(6).min(6),
+    confirmPin: z.string().max(6).min(6),
+  })
+  .refine(data => data.pin === data.confirmPin, {
+    message: "Pins don't match",
+    path: ['confirmPin'],
+  });
+
+const defaultValues = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  phoneNumber: '',
+  pin: '',
+  confirmPin: '',
+  checked: false,
+};
+
+type FormFields = z.infer<typeof schema>;
+
 const CreateAccount = () => {
+  const [termsChecked, setTermsChecked] = useState(false);
+  const {
+    control,
+    handleSubmit,
+    formState: {isValid, errors},
+  } = useForm<FormFields>({
+    mode: 'onChange',
+    defaultValues,
+    resolver: zodResolver(schema),
+  });
+
+  const handleCheckboxChange = (checked: boolean) => {
+    setTermsChecked(checked);
+  };
+
+  const onSubmit: SubmitHandler<FormFields> = data => {};
+
   return (
     <React.Fragment>
       <div className="mb-space600">
@@ -14,20 +61,90 @@ const CreateAccount = () => {
           Join the league of happy people using Ashford for their deliveries
         </p>
       </div>
-      <form className=" h-[65vh] flex flex-col justify-between ">
+      <form
+        className=" h-[65vh] flex flex-col justify-between"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <div className="flex flex-col justify-between gap-space200">
           <div className="flex gap-space200">
-            <Input placeholder="First Name" />
-            <Input placeholder="Last Name" />
+            <Controller
+              name="firstName"
+              control={control}
+              render={({field}) => (
+                <Input
+                  {...field}
+                  errorMessage={errors.firstName?.message}
+                  placeholder="First Name"
+                />
+              )}
+            />
+
+            <Controller
+              name="lastName"
+              control={control}
+              render={({field}) => (
+                <Input
+                  {...field}
+                  errorMessage={errors.lastName?.message}
+                  placeholder="Last Name"
+                />
+              )}
+            />
           </div>
-          <Input placeholder="Phone number" />
+          <Controller
+            name="email"
+            control={control}
+            render={({field}) => (
+              <Input
+                {...field}
+                errorMessage={errors.email?.message}
+                placeholder="Email"
+              />
+            )}
+          />
+          <Controller
+            name="phoneNumber"
+            control={control}
+            render={({field}) => (
+              <Input
+                {...field}
+                errorMessage={errors.phoneNumber?.message}
+                placeholder="Phone number"
+              />
+            )}
+          />
+
           <Input placeholder="Name of company (Optional)" />
-          <Input placeholder="Create a 6-digit pin" type="password" />
-          <Input placeholder="Confirm pin" type="password" />
+          <Controller
+            name="pin"
+            control={control}
+            render={({field}) => (
+              <Input
+                {...field}
+                errorMessage={errors.pin?.message}
+                placeholder="Create a 6-digit pin"
+              />
+            )}
+          />
+          <Controller
+            name="confirmPin"
+            control={control}
+            render={({field}) => (
+              <Input
+                {...field}
+                errorMessage={errors.confirmPin?.message}
+                placeholder="Confirm pin"
+              />
+            )}
+          />
         </div>
         <div>
           <div className="items-top flex space-x-2 mb-2">
-            <Checkbox id="terms1" />
+            <Checkbox
+              id="terms1"
+              checked={termsChecked}
+              onCheckedChange={handleCheckboxChange}
+            />
             <div className="grid gap-1.5 leading-none">
               <label
                 htmlFor="terms1"
@@ -42,7 +159,9 @@ const CreateAccount = () => {
             </div>
           </div>
           <div className="flex flex-col text-center gap-space100">
-            <Button className="w-full">Submit</Button>
+            <Button className="w-full" disabled={!termsChecked || !isValid}>
+              Submit
+            </Button>
             <span className="text-[0.8rem]">
               Have an account already? <a href="/auth/login">Login</a>
             </span>
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,8 +2,11 @@ import axios, {RawAxiosRequestHeaders, AxiosInstance} from 'axios';
 
 // create an axios instance
 export const api: AxiosInstance = axios.create({
-  baseURL: 'https://example.com/v1/',
-  headers: {} as RawAxiosRequestHeaders,
+  baseURL: process.env.REACT_APP_BASEURL,
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+  } as RawAxiosRequestHeaders,
 });
 
 // request interceptor
